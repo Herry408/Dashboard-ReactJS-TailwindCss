848a4197401badccf99b33a17d6b35ec9fd50441
Heri Suartawan <suartawan.herry@gmail.com>
2021-09-13 17:47:41 +0800

    add content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import NavbarComponent from "./components/NavbarComponent";
 import SidebarComponents from "./components/SidebarComponents";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div>
       <NavbarComponent />
-      <SidebarComponents />
+      <div class="relative min-h-screen flex">
+        <SidebarComponents />
+        <div class="p-10">
+          <Content />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Content() {
-  return <div></div>;
+  return (
+    <div class="w-full">
+      Amet sint tempor veniam consequat ad laboris. Commodo proident dolor ex
+      magna nostrud. Eu ut minim amet cupidatat nisi sint pariatur pariatur
+      aliquip sunt non. Eiusmod est dolore aute cillum consequat Lorem minim qui
+      nisi fugiat Lorem officia anim sunt. Ad non do mollit elit incididunt
+      labore minim nostrud fugiat. Quis pariatur proident anim exercitation
+      reprehenderit culpa ex qui incididunt eiusmod adipisicing non et ut.
+      Nostrud in quis Lorem ullamco velit reprehenderit laboris esse veniam
+      deserunt.
+    </div>
+  );
 }
